c1aed43bb14bf30bf47202ed740150dc2009dff2
anastasiiaxfr <anastasiiaberest@gmail.com>
2023-02-20 12:03:41 +0200

    content-types

---
+++ src/api/beginner/controllers/beginner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * beginner controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::beginner.beginner');
