6430f57a61b2803883282e8b67fd877bfd0f7b3b
anastasiiaxfr <anastasiiaberest@gmail.com>
2023-03-06 12:06:04 +0200

    fix

--- fe/next-sitemap.config.js
+++ fe/next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'http://localhost:3000/',
     generateRobotsTxt: true, // (optional)
+    generateIndexSitemap: false,
     changefreq: 'daily',
     priority: 0.7,
     sitemapSize: 5000,
